e316294c4137418254a6d4c2df347302a9f0b202
Fuc le <quangphuchoctapit@gmail.com>
2023-11-05 10:29:39 +0700

    search feature

--- crud/nav/nav.php
+++ crud/nav/nav.php
@@ -44,17 +44,17 @@ $thisUserId = $_SESSION['user_id'];
                         <?php
                         // echo '<pre>' . print_r($_SESSION, TRUE) . '</pre>';
                         if ($_SESSION['user_id'] != $_SESSION['admin_id']) { ?>
-                        <div class='category-item category-item-3'>
-                            <a href='/comp1841/crud/user/contactAdmin.php'>
-                                Contact Administrator
-                            </a>
-                        </div>
+                            <div class='category-item category-item-3'>
+                                <a href='/comp1841/crud/user/contactAdmin.php'>
+                                    Contact Administrator
+                                </a>
+                            </div>
                         <?php } else { ?>
-                        <div class='category-item category-item-3'>
-                            <a href='/comp1841/admin/contactUser.php'>
-                                Manage Users' Request
-                            </a>
-                        </div>
+                            <div class='category-item category-item-3'>
+                                <a href='/comp1841/admin/contactUser.php'>
+                                    Manage Users' Request
+                                </a>
+                            </div>
                         <?php }
                         ?>
                     </div>
@@ -69,9 +69,9 @@ $thisUserId = $_SESSION['user_id'];
 
                 <?php
                 if ($_SESSION['user_id'] == $_SESSION['admin_id']) { ?>
-                <div class='nav-child'><a class='text-decoration-none' href='/comp1841/admin/displayUser.php'>View List
-                        Users</a>
-                </div>
+                    <div class='nav-child'><a class='text-decoration-none' href='/comp1841/admin/displayUser.php'>View List
+                            Users</a>
+                    </div>
                 <?php    }
                 ?>
 
@@ -86,79 +86,73 @@ $thisUserId = $_SESSION['user_id'];
                         Modules</a>
                 </div>
                 <div class="nav-child-chat">
-                    <button class="btnChat-nav" onclick="openChat()">Chat
+                    <button class="btnChat-nav" onclick="openChat()"><i class="far fa-comment"></i>
+                    </button>
+                </div>
+                <div class="nav-child-search">
+                    <button class="btnSearch-nav"><a href='/comp1841/crud/search/search.php'><i class="fas fa-search"></i></a>
                     </button>
                 </div>
 
             </div>
             <div class="nav-child-right">
                 <?php if ($_SESSION['user_id'] != $_SESSION['admin_id']) { ?>
-                <div class="admin-notification" onclick="seenNotification()">
-                    <a href='/comp1841/crud/user/adminReply.php' style='color: #fff'>
-                        Notifications
-                        <?php
-                            if (array_key_exists('admin_seen', $_SESSION)) {
-                                $unread_admin = $_SESSION['admin_seen'];
-                                if ($unread_admin == 'unread') {
-                            ?>
-                        <div class='dot-notification'></div>
-                    </a>
-                    <?php
-                                }
-                            }
-                        }
-            ?>
-                    <div class="nav-user-avt">
-                        <a href="/comp1841/crud/user/userInfo.php?userId=<?php echo $thisUserId; ?>">
-                            <?php
-                    $userId = $_SESSION['user_id'];
-                    $sql = "select `image` from `user` where id = $userId;";
-                    $result = $conn->query($sql);
-                    $d = $result->fetch();
-                    $img = $d['image'];
-                    if ($img) { ?>
-                            <div class="nav-user-avt-img"
-                                style="background: transparent url(/comp1841/crud/user/uploads/<?php echo $img; ?>) center center no-repeat; height: 30px; width: 30px; padding: 3px;background-size: contain">
-                            </div>
-                            <?php } else { ?>
-                            <div class="nav-user-avt-img"
-                                style="background: transparent url(/comp1841/crud/user/uploads/IMG-653751dd87d0c4.57015077.png) center center no-repeat; height: 30px; width: 30px; padding: 3px;background-size: contain">
-                            </div>
-                            <?php }
-                    ?>
+                    <div class="admin-notification" onclick="seenNotification()">
+                        <a href='/comp1841/crud/user/adminReply.php' style='color: #fff'>
+                            Notifications
                         </a>
                     </div>
-                    <span>Hello, <?php echo $user_data['name']; ?></span>
-                    <div class="logout">
-                        <a class='logout-a' href="/comp1841/auth/login.php">Logout &nbsp;<i
-                                class="fas fa-sign-out-alt"></i></a>
-                    </div>
-
+                <?php
+                }
+                ?>
+                <div class="nav-user-avt">
+                    <a href="/comp1841/crud/user/userInfo.php?userId=<?php echo $thisUserId; ?>">
+                        <?php
+                        $userId = $_SESSION['user_id'];
+                        $sql = "select `image` from `user` where id = $userId;";
+                        $result = $conn->query($sql);
+                        $d = $result->fetch();
+                        $img = $d['image'];
+                        if ($img) { ?>
+                            <div class="nav-user-avt-img" style="background: transparent url(/comp1841/crud/user/uploads/<?php echo $img; ?>) center center no-repeat; height: 30px; width: 30px; padding: 3px;background-size: contain">
+                            </div>
+                        <?php } else { ?>
+                            <div class="nav-user-avt-img" style="background: transparent url(/comp1841/crud/user/uploads/IMG-653751dd87d0c4.57015077.png) center center no-repeat; height: 30px; width: 30px; padding: 3px;background-size: contain">
+                            </div>
+                        <?php }
+                        ?>
+                    </a>
                 </div>
-                <div id="chatArea" style='display:none'>
-                    <?php include '/xampp/htdocs/comp1841/chat/homeChat.php'; ?>
+                <span>Hello, <?php echo $user_data['name']; ?></span>
+                <div class="logout">
+                    <a class='logout-a' href="/comp1841/auth/login.php">Logout &nbsp;<i class="fas fa-sign-out-alt"></i></a>
                 </div>
-
 
             </div>
+            <div id="chatArea" style='display:none'>
+                <?php include '/xampp/htdocs/comp1841/chat/homeChat.php'; ?>
+            </div>
+
+
         </div>
+    </div>
 
     </div>
     <script src='/comp1841/crud/nav/nav.js'></script>
     <script>
-    function openChat() {
-        let areaText = document.getElementById('chatArea');
-        let btnChat = document.getElementsByClassName('btnChat');
-        if (areaText.style.display === "none") {
-            areaText.style.display = "block";
+        function openChat() {
+            let areaText = document.getElementById('chatArea');
+            let btnChat = document.getElementsByClassName('btnChat');
+            if (areaText.style.display === "none") {
+                areaText.style.display = "block";
+            }
         }
-    }
 
-    function seenNotification() {
-        let notificationBtn = document.querySelector('.admin-notification');
-        let dotNotification = document.querySelector('.dot-notification');
-        <?php $unread_admin = 'read'; ?>
-    }
+        function seenNotification() {
+            let notificationBtn = document.querySelector('.admin-notification');
+            let dotNotification = document.querySelector('.dot-notification');
+            <?php $unread_admin = 'read'; ?>
+        }
     </script>
 </body>
 
---
+++ crud/search/search.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="/comp1841/crud/search/search.css?v=<?php echo time(); ?>" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.7/css/all.css">
+    <title>Search</title>
+</head>
+
+<body>
+    <?php
+    include "/xampp/htdocs/comp1841/crud/nav/nav.php";
+    if (array_key_exists('user-input', $_POST)) {
+        $user_input = $_POST['user-input'];
+    } else {
+        $user_input = '';
+    }
+
+    ?>
+
+
+    <form method='POST'>
+        <div class="searchArea-body">
+            <div class="search-container-homeSearch">
+                <div class="up-container">
+                    <div class="inputSearchUser">
+                        <input type="text" placeholder="Search..." name='user-input' id="searchTextInput"
+                            value='<?php echo $user_input; ?>' class="form-control">
+                    </div>
+                </div>
+                <div class="down-container">
+                    <div class="down">
+                        <div class="content">
+                            <div id="search-option">
+                                <div class="no-message">
+                                    <input type='submit' name='user' class="btn-option" value='Search User' />
+                                </div>
+                                <div class="no-message">
+                                    <input type='submit' name='post' class="btn-option" value='Search Post' />
+                                </div>
+                                <div class="no-message">
+                                    <input type='submit' name='module' class="btn-option" value='Search Module' />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="result-search">
+                <?php
+                if (isset($_POST['user'])) {
+                    $resultUser = $conn->query("select `id`, `name`, `email`, `image` from `user` where name like '%$user_input%' or email like '%$user_input%@gmail.com' ;");
+                    $dUser = $resultUser->fetchAll();
+                    if ($dUser) {
+                        foreach ($dUser as $row) {
+                            $name = $row['name'];
+                            $id = $row['id'];
+                            $email = $row['email'];
+                            $image = $row['image'];
+                ?>
+                <a href="/comp1841/crud/user/userInfo.php?userId=<?php echo $id; ?>">
+                    <div class="each-user-found">
+                        <div class="user-avt"
+                            style='background: url(/comp1841/crud/user/uploads/<?php echo $image; ?>) center center no-repeat; background-size: contain; border-radius: 50%;'>
+                        </div>
+                        <div class="user-name"><?php echo $name; ?></div>
+                        <div class="user-email"><?php echo $email; ?></div>
+                    </div>
+                </a>
+                <?php
+                        }
+                    }
+                } elseif (isset($_POST['post'])) {
+                    $resultPost = $conn->query("select id, title, details, module from `post` where `title` like '%$user_input%' or `details` like '%$user_input%';");
+                    $dPost = $resultPost->fetchAll();
+                    if ($dPost) {
+                        foreach ($dPost as $row) {
+                            $title = $row['title'];
+                            $id = $row['id'];
+                            $details = $row['details'];
+                            $moduleId = $row['module'];
+                            $resultGetModule = $conn->query("select * from `module` where id=$moduleId");
+                            $dataResultGetModule = $resultGetModule->fetch();
+                            $module_name = $dataResultGetModule['module_name'];
+                            $module_id = $dataResultGetModule['module_id'];
+                        ?>
+                <a href="/comp1841/crud/home/home.php?postId=<?php echo $id; ?>">
+                    <div class="each-post-found">
+                        <div class="post-title">
+                            <?php echo $title; ?>
+                        </div>
+                        <div class="post-details"><?php echo $details; ?></div>
+                        <div class="post-module-id"><?php echo $module_id; ?></div>
+                    </div>
+                </a>
+                <?php
+                        }
+                    }
+                } elseif (isset($_POST['module'])) {
+                    $resultModule = $conn->query("SELECT * FROM `module` WHERE `module_name` like '%$user_input' or `module_id` like '%$user_input%';");
+                    $dModule = $resultModule->fetchAll();
+                    if ($dModule) {
+                        foreach ($dModule as $row) {
+                            $module_name = $row['module_name'];
+                            $module_id = $row['module_id'];
+                        ?>
+                <div class="each-module-found">
+                    <div class="module_name"><?php echo $module_name; ?></div>
+                    <div class="module_id"><?php echo $module_id; ?></div>
+                </div>
+                <?php
+                        }
+                    }
+                }
+                ?>
+            </div>
+
+        </div>
+    </form>
+
+</body>
+
+</html>
