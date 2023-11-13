45ca3f102161aca01524d038ef036acaba9cc610
Fuc le <quangphuchoctapit@gmail.com>
2023-11-13 14:15:12 +0700

    more css

--- crud/user/contactAdmin.php
+++ crud/user/contactAdmin.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="/comp1841/crud/askPage/askPage.css" />
+    <link rel="stylesheet" type="text/css" href="/comp1841/crud/askPage/askPage.css?v=<?php echo time(); ?>" />
     <link rel="stylesheet" type="text/css" href="/comp1841/crud/home/home.css?v=<?php echo time(); ?>" />
     <title>Contact Admin</title>
 </head>
